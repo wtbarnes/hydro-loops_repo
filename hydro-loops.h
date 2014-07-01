fc539492d6b6ddf32c3b4f4ce85e052b9820fe07
Will Barnes <willbarnes@will-barness-macbook-pro.local>
2014-06-30 22:51:16 -0500

    Created header file

---
+++ hydro-loops.h
@@ -0,0 +1,21 @@
+#ifndef hydroloops_H
+#define hydroloops_H
+
+#include <stdlib.h>
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include <string.h>
+#include <time.h>
+#include <float.h>
+
+//Declare some global variables
+extern double RSOL;
+extern double GSOL;
+extern double KB;
+extern double KAPPA;
+extern double MP;
+extern double MU;
+extern double PI;
+
+#endif
